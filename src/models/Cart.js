3ff987d091686b31fa37277f6e41dc9c00723dc5
max <maxpete121@gmail.com>
2023-12-21 10:53:34 -0700

    price update

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -6,6 +6,7 @@ import { Schema } from "mongoose";
 
 export const CartSchema = new Schema({
     name: {type: String, maxLength: 35, required: true},
+    price: {type: String, maxLength: 30, required: true},
     description: {type: String, maxLength: 200, required: true},
     imgUrl: {type: String, maxLength: 300, required: true},
     specsId: {type: Schema.Types.ObjectId, ref: 'Specs', required: true}
--- src/models/Item.js
+++ src/models/Item.js
@@ -6,6 +6,7 @@ import { SpecsSchema } from "./Specs.js";
 
 export const ItemSchema = new Schema({
     name: {type: String, maxLength: 35, required: true},
+    price: {type: String, maxLength: 30, required: true},
     description: {type: String, maxLength: 200, required: true},
     imgUrl: {type: String, maxLength: 300, required: true},
     specsId: {type: Schema.Types.ObjectId, ref: 'Specs', required: true}
